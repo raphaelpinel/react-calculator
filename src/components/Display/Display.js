76dc2b4abe2ce579c893dce68d3fd0d1f0576db6
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-03 14:00:40 +0200

    improved display styling

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,12 +3,15 @@ import styles from './Display.module.css';
 
 const Display = props => {
   let display = props.display;
+  if (display.length > 9) {
+    display = Number.parseFloat(props.display).toExponential(7);
+  }
   let bigNumberStyle = null;
   if (display.length >= 10) {
     bigNumberStyle = { fontSize: '40px' };
   }
   if (display.length >= 7 && display.length < 10) {
-    bigNumberStyle = { fontSize: '55px' };
+    bigNumberStyle = { fontSize: '50px' };
   }
   function add3digitsSeparator(x) {
     var parts = x.toString().split('.');
