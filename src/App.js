ebe0b21b3b973d3c6b538ee6731a82a76baa6821
Raphael Pinel <raphaelpinel8@gmail.com>
2019-02-22 10:44:34 +0200

    working active classes toggle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import safeEval from 'safe-eval';
-import styles from './App.module.css';
+import React, { Component } from "react";
+import safeEval from "safe-eval";
+import classNames from "classnames";
+import styles from "./App.module.css";
 
-import Display from './components/Display/Display.js';
-import Button from './components/Button/Button';
+import Display from "./components/Display/Display.js";
+import Button from "./components/Button/Button";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: '0', // the value displayed
+      display: "0", // the value displayed
       precedence: 0,
       waiting: 0,
       memory: [], // where the numbers and operators are stored
-      resetDisplay: false, // set to true when the user has entered an operator or "=", 
+      resetDisplay: false, // set to true when the user has entered an operator or "=",
       //then immediately back to false so s/he can continue entering a number
-      resetButton: 'AC'
+      resetButton: "AC",
+      operator: ""
     };
   }
 
@@ -23,9 +25,9 @@ class App extends Component {
     if (val) {
       return Number.parseFloat(val.toFixed(15));
     }
-    return '0';
+    return "0";
   };
-  
+
   safeEval2 = val => this.fixFloatingPoint(safeEval(val));
 
   numberClick = event => {
@@ -33,101 +35,159 @@ class App extends Component {
     let value = event.currentTarget.dataset.value; //the digit the user typed
     let newDisplay = display + value;
 
-    if (value !== '0') {
-      this.setState({ resetButton: 'C' });
+    if (value !== "0") {
+      this.setState({ resetButton: "C" });
     }
 
-    if(display === '0' && value !== '.') {
+    if (display === "0" && value !== ".") {
       // replaces the zero by the actual value
       newDisplay = value;
     }
-    if(value === '.' && display.includes('.') ) {
+    if (value === "." && display.includes(".")) {
       // prevents multiple '.'
       newDisplay = display;
     }
     if (resetDisplay) {
-      newDisplay = value === '.' ? '0.' : value;
+      newDisplay = value === "." ? "0." : value;
       // reset InputValue to allow user to enter 2nd part number after operator and stores the first part value to FirstPartValue.
       this.setState({
         resetDisplay: false
       });
     }
     this.setState({
-      display: newDisplay,
+      display: newDisplay
     });
   };
 
+  active = operator => {
+    return this.state.resetDisplay && operator === this.state.operator
+      ? "active"
+      : "";
+  };
+
+  toggleActive = event => {
+    const { classList } = event.currentTarget;
+    if (classList.value.includes("active")) {
+      classList.remove("active");
+    } else {
+      classList.add("active");
+    }
+  };
+
+  addActive = event => event.currentTarget.classList.add("active");
+
+  removeActive = event => event.currentTarget.classList.remove("active");
+
   operatorClick = event => {
-    event.currentTarget.classList.add('active');
-    let { display, precedence, memory, resetDisplay, waiting } = this.state
+    //this.toggleActive(event);
+    const { classList } = event.currentTarget;
+
+    let { display, precedence, memory, resetDisplay, waiting } = this.state;
     const value = event.currentTarget.dataset.value; // the operator entered by user
-    
+
     const operators = [
-      {value: '+', operation(a, b) {return a + b}, precedence: 1},
-      {value: '-', operation(a, b) {return a - b}, precedence: 1},
-      {value: '*', operation(a, b) {return a * b}, precedence: 2},
-      {value: '/', operation(a, b) {return a / b}, precedence: 2},
-      {value: '='},
-      {value: '%'},
+      {
+        value: "+",
+        operation(a, b) {
+          return a + b;
+        },
+        precedence: 1
+      },
+      {
+        value: "-",
+        operation(a, b) {
+          return a - b;
+        },
+        precedence: 1
+      },
+      {
+        value: "*",
+        operation(a, b) {
+          return a * b;
+        },
+        precedence: 2
+      },
+      {
+        value: "/",
+        operation(a, b) {
+          return a / b;
+        },
+        precedence: 2
+      },
+      { value: "=" },
+      { value: "%" }
     ];
-    
-    const selectedOperator = operators.filter(operator => operator.value === value)[0];
 
-    if (selectedOperator.value === '=' && !(resetDisplay && (/\d$/).test(memory.join('')))) {
+    const selectedOperator = operators.filter(
+      operator => operator.value === value
+    )[0];
+
+    if (
+      selectedOperator.value === "=" &&
+      !(resetDisplay && /\d$/.test(memory.join("")))
+    ) {
       //prevent multiple equals
-      const preResult = resetDisplay ? memory.join('').slice(0, -1) : memory.join('') + display ;
+      const preResult = resetDisplay
+        ? memory.join("").slice(0, -1)
+        : memory.join("") + display;
       const result = preResult ? this.safeEval2(preResult).toString() : display;
-      this.setState({memory: [], display: result});
-
-    } else if (selectedOperator.value === '%') {
+      this.setState({ memory: [], display: result });
+    } else if (selectedOperator.value === "%") {
       let result;
-      if ((/[+-]/).test(memory[memory.length -1])) {
-        const base = memory.join('').slice(0, -1);
-        const finalOperator = memory.join('').slice(-1)
-        result = this.safeEval2(`${base}${finalOperator}${base}*${display}/100`);
+      if (/[+-]/.test(memory[memory.length - 1])) {
+        const base = memory.join("").slice(0, -1);
+        const finalOperator = memory.join("").slice(-1);
+        result = this.safeEval2(
+          `${base}${finalOperator}${base}*${display}/100`
+        );
       } else {
         result = this.safeEval2(`${display}/100`).toString();
       }
-      this.setState({display: result.toString(), memory: []});
-
+      this.setState({ display: result.toString(), memory: [] });
     } else if (memory.length === 0) {
-      this.setState({memory: memory.concat(display, value)});
-
+      this.setState({ memory: memory.concat(display, value) });
+      this.addActive(event);
     } else if (resetDisplay) {
       // if the user enters many times an operator, replace by the last one except if it is equal
-      memory[memory.length -1] = memory[memory.length -1].slice(0, -1) + value;
-      this.setState({memory});
-
+      memory[memory.length - 1] =
+        memory[memory.length - 1].slice(0, -1) + value;
+      this.setState({ memory });
     } else if (selectedOperator.precedence === precedence && waiting !== 0) {
       // ex. 1+2*3* //6 // partial evaluate: memory was ["1+", "2*"] => ["1+", "6*"]
-      const index = memory.length - 1 // 1 // could be also memory[waiting] 
-      const result =  this.safeEval2(memory[index] + display).toString();
+      const index = memory.length - 1; // 1 // could be also memory[waiting]
+      const result = this.safeEval2(memory[index] + display).toString();
       memory[index] = result + value;
       this.setState({ display: result, memory });
-
     } else if (selectedOperator.precedence <= precedence) {
       // calculate all, ex: 2*2+ or 1+2*3+ // attention 1+2*3*5+ // should be 31
-      const result =  this.safeEval2(memory.concat(display).join('')).toString();
-      this.setState({memory: [result].concat(value), display: result, waiting: 0});
-
+      const result = this.safeEval2(memory.concat(display).join("")).toString();
+      this.setState({
+        memory: [result].concat(value),
+        display: result,
+        waiting: 0
+      });
     } else if (selectedOperator.precedence > precedence && waiting === 0) {
       // case 1+2* // need to wait: waiting++ // memory should be ["1+", "2*"]
-      this.setState({ waiting: waiting +1, memory: memory.concat(display + value)});
-
+      this.setState({
+        waiting: waiting + 1,
+        memory: memory.concat(display + value)
+      });
     } else {
-      this.setState({display: 'error'});
+      this.setState({ display: "error" });
       alert("error !!!");
     }
-      return this.setState({
-        precedence: selectedOperator.precedence,
-        resetDisplay: true,
-      });
+    return this.setState({
+      precedence: selectedOperator.precedence,
+      resetDisplay: true,
+      operator: value
+    });
   };
 
   inverse = () => {
     const { display } = this.state;
     // checks if the number starts already with a minus sign to remove it, otherwise adds it
-    const newDisplay = (display.charAt(0) === '-') ? display.substr(1) : `-${display}`;
+    const newDisplay =
+      display.charAt(0) === "-" ? display.substr(1) : `-${display}`;
     this.setState({
       display: newDisplay
     });
@@ -135,82 +195,128 @@ class App extends Component {
 
   reset = () => {
     const { resetButton } = this.state;
-    if (resetButton === 'C') {
-      return this.setState({display: '0', resetButton: 'AC'});
-    } 
+    if (resetButton === "C") {
+      return this.setState({ display: "0", resetButton: "AC" });
+    }
     this.setState({
-      display: '0',
+      display: "0",
       memory: [],
       resetDisplay: false,
       precedence: 0,
       waiting: 0,
-      resetButton: 'AC',
+      resetButton: "AC"
     });
   };
 
   render() {
     const { display, resetButton } = this.state;
+    const btnClass = "Button";
     return (
       <div className={styles.App}>
         <Display display={display} />
         <div className="Buttons">
           <Button
+            className={classNames(btnClass, "lightgrey")}
             click={this.reset}
             value={resetButton}
             name="AC"
-            special="lightgrey"
           />
           <Button
+            className={classNames(btnClass, "lightgrey")}
             click={this.inverse}
             value="+/-"
-            special="lightgrey"
           />
           <Button
+            className={classNames(btnClass, "lightgrey")}
             click={this.operatorClick}
             value="%"
-            special="lightgrey"
           />
           <Button
+            className={classNames(btnClass, "orange", this.active("/"))}
             click={this.operatorClick}
-                        value="/"
-            special="orange"
+            value="/"
           />
 
-          <Button click={this.numberClick} value="7" />
-          <Button click={this.numberClick} value="8" />
-          <Button click={this.numberClick} value="9" />
           <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="7"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="8"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="9"
+          />
+          <Button
+            className={classNames(btnClass, "orange", this.active("*"))}
             click={this.operatorClick}
-            value="*" label="x"
-            special="orange"
+            value="*"
+            label="x"
           />
 
-          <Button click={this.numberClick} value="4" />
-          <Button click={this.numberClick} value="5" />
-          <Button click={this.numberClick} value="6" />
           <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="4"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="5"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="6"
+          />
+          <Button
+            className={classNames(btnClass, "orange", this.active("-"))}
             click={this.operatorClick}
             value="-"
-            special="orange"
-            //active={true}
           />
 
-          <Button click={this.numberClick} value="1" />
-          <Button click={this.numberClick} value="2" />
-          <Button click={this.numberClick} value="3" />
           <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="1"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="2"
+          />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="3"
+          />
+          <Button
+            className={classNames(btnClass, "orange", this.active("+"))}
             click={this.operatorClick}
             value="+"
-            special="orange"
           />
 
           <Button
+            className={classNames(btnClass, "double")}
             click={this.numberClick}
             value="0"
-            special="double"
           />
-          <Button click={this.numberClick} value="." />
-          <Button click={this.operatorClick} value="=" special="orange" />
+          <Button
+            className={classNames(btnClass)}
+            click={this.numberClick}
+            value="."
+          />
+          <Button
+            className={classNames(btnClass, "orange")}
+            click={this.operatorClick}
+            value="="
+            special="orange"
+          />
         </div>
       </div>
     );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import styles from './Button.module.css';
+//import styles from './Button.module.css';
+import './Button.css';
 
 const Button = (props: any) => {
-  const classesArray = [styles.Button];
+  // const classesArray = [styles.Button];
+  // if (props.special) {
+  //   const specialStyle = props.special;
+  //   classesArray.push(styles[specialStyle], props.active ? styles.active : ''); //adding special css class in order to make buttons orange or lightgrey
+  // }
+  const classesArray = ['Button'];
   if (props.special) {
-    const specialStyle = props.special;
-    classesArray.push(styles[specialStyle], props.active ? styles.active : ''); //adding special css class in order to make buttons orange or lightgrey
+    classesArray.push(props.special, props.active ? 'active' : '');
   }
-
   return (
     <div
-      className={classesArray.join(' ')}
+      //className={classesArray.join(' ')}
+      //className={classesArray.join(' ')}
+      className={props.className}
       data-value={props.value}
       onClick={props.click}
     >
